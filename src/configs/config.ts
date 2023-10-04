8c088f1a32b7574636d23546c8efcc0f67807e6e
killerio22 <vladkaraman14@gmail.com>
2023-10-04 12:41:48 +0300

    hw5

---
+++ src/configs/config.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+
+config();
+
+export const configs = {
+  DB_URI: process.env.DB_URI || "mongodb://localhost:27017/march-2023",
+  PORT: process.env.PORT || 5001,
+  SECRET_SALT: process.env.SECRET_SALT,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+};
